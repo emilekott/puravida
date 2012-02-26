16d3d3331724b6538d1249021d7ed1d8f21d24a3
Emile Kott <emilekott@gmail.com>
2012-02-26 12:37:32 +0000

    installed node slideshow

---
+++ sites/all/modules/node_slideshow/script.js
@@ -0,0 +1,10 @@
+(function($) {
+$(document).ready(function(){
+
+   $('.content .field-name-field-slideshow-index .field-items').cycle('fade');
+   
+ 
+});
+    
+    
+})(jQuery);//the correct way of namespacing jquery

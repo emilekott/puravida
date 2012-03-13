0f4f24a844aef94406cbc4248d4db50b3f958eff
Emile Kott <emilekott@gmail.com>
2012-03-13 19:21:39 +0000

    category page theming

--- sites/all/themes/puravidax/template.php
+++ sites/all/themes/puravidax/template.php
@@ -64,3 +64,27 @@ $breadcrumb = $variables['breadcrumb'];
         return $crumbs;
     }
 }
+
+function puravidax_form_alter(&$form, &$form_state, $form_id) {
+    //theme add to cart buttons etc.
+    if (preg_match('/uc_catalog_buy_it_now_form/', $form_id)){
+        $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path().'/sites/all/themes/puravidax/images/buy-now.jpg');
+    }
+    /*
+    if ($form_id == 'search_block_form') {
+        $form['search_block_form']['#title'] = t('Search'); 
+        $form['search_block_form']['#title_display'] = 'invisible'; 
+        $form['search_block_form']['#size'] = 20;  
+        $form['search_block_form']['#default_value'] = t('Search the site...'); 
+        // $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+        $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
+
+        
+        $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search the site...';}";
+        $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search the site...') {this.value = '';}";
+    }
+    if ($form_id == 'user_login_block')
+        unset($form['links']);
+
+*/
+}
